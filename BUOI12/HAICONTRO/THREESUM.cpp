aa6b1211672e7ccbd4e7300b71fbaa3c4d6bfedf
tlowng <121792617+longathelstan@users.noreply.github.com>
2024-11-30 07:46:18 +0700

    Update THREESUM.cpp

--- BUOI12/HAICONTRO/THREESUM.cpp
+++ BUOI12/HAICONTRO/THREESUM.cpp
@@ -1,31 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int limit = 1e5+1;
+const int limit = 1e5 + 1;
 
 int main() {
     int n;
     ll s;
     pair<ll, int> a[limit];
     cin >> n >> s;
+
     for (int i = 1; i <= n; i++) {
         cin >> a[i].first;
         a[i].second = i;
     }
-    int i = 1, j = n;
+
     sort(a + 1, a + n + 1);
-    for (int b = 1; b <= n; i++) {
+
+    for (int b = 1; b <= n; b++) {
         ll t = s - a[b].first;
+        int i = 1, j = n;
+
         while (i < j) {
-        if (a[i].first + a[j].first == t && i != b && j != b) {
-            cout << a[b].second << " " << a[i].second << " " << a[j].second;
-            return 0;
-        } else {
-            if (a[i].first + a[j].first < t) i++;
-            else j--;
-        }
+            if (i == b) {
+                i++;
+                continue;
+            }
+            if (j == b) {
+                j--;
+                continue;
+            }
+            if (a[i].first + a[j].first == t) {
+                cout << a[b].second << " " << a[i].second << " " << a[j].second;
+                return 0;
+            } else if (a[i].first + a[j].first < t) {
+                i++;
+            } else {
+                j--;
+            }
         }
     }
-
     cout << "IMPOSSIBLE";
+    return 0;
 }
