94e58f4f29da9c41b62364b34cfafc98b268c935
LongAthelstan <121792617+longathelstan@users.noreply.github.com>
2025-01-03 13:23:41 +0700

    awlongg

--- BUOI26/KIEMTRA4/UOC.cpp
+++ BUOI26/KIEMTRA4/UOC.cpp
@@ -1,41 +1,24 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int limit = 1e6+8;
-bool prime[limit];
-vector<ll> v;
-void sang() {
-    fill(prime, prime + limit, true);
-    prime[0] = prime[1] = false;
-    for (int i = 2; i <= sqrt(limit); i++) {
-        if (prime[i]) for (int j = i * i; j <= limit; j += i) prime[j] = false;
-    }
-    for (int i = 2; i <= limit; i++) {
-        if (prime[i]) v.push_back(i);
-    }
-}
 
 ll dha(ll n) {
-    ll kq = 1;
-    for (auto p : v) {
-        if (p * p > n) break;
-        ll dem = 0;
-        while (n % p == 0) {
-            n /= p;
+    ll dem = 0;
+    for (ll i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
             dem++;
+            if (n / i != i) dem++;
         }
-        kq *= (dem + 1);
     }
-    if (n > 1) kq *= 2;
-    return kq;
+    return dem;
 }
 
 int main() {
     freopen("UOC.INP", "r", stdin);
     freopen("UOC.OUT", "w", stdout);
     ios::sync_with_stdio(false);
-cin.tie(0);
-cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
     sang();
     ll n;
     cin >> n;
--- BUOI27/FOURDIV.cpp
+++ BUOI27/FOURDIV.cpp
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int limit = 1e6+1;
-int boi[limit] = {0};
-
-void sangboi() {
-    for (int i = 1; i <= limit; i++) {
-        for (int j = i; j <= limit; j += i) {
-            boi[j]++;
+const int limit = 1e4+8;
+bool awl(ll n, ll d) {
+    vector<ll> v;
+    for (ll i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            v.push_back(i);
+            if (i != n / i) {
+                v.push_back(n / i);
+            }
         }
     }
+    if (v.size() < 4) return false;
+    sort(v.begin(), v.end());
+    for (ll i = 1; i < v.size(); i++) {
+        if (v[i] - v[i - 1] < d) return false;
+    }
+    return true;
 }
+
 int main() {
-    sangboi();
     ll t, d;
-//    cin >> t;
-//    while(t--) {
+    cin >> t;
+    while(t--) {
         cin >> d;
-        for (int i = 1; i <= limit; i++) {
-            ll k = i + d;
-            if (boi[k * i] >= 4) {
-                cout << k * i << endl;
+        for (ll i = 1; i <= limit; i++) {
+            if (awl(i, d)) {
+                cout << i << "\n";
                 break;
             }
         }
-//    }
+    }
 }
+
