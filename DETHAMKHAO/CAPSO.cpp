31c758079bb7b4f004fc28a1720fa50a968ce332
longathelstan <121792617+longathelstan@users.noreply.github.com>
2025-04-06 22:29:10 +0700

    yessir

--- DETHAMKHAO/CAPSO.cpp
+++ DETHAMKHAO/CAPSO.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int limit = 1e6+8;
 
+int main() {
+    int n, a[limit], d[limit] = {0}, maxx = INT_MIN, dem = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        d[a[i]]++;
+        maxx = max(maxx, a[i]);
+    }
+    for (int i = 1; i <= maxx; i++) {
+        if (d[i] > 1) dem += d[i] * (d[i] - 1) / 2;
+    }
+    cout << dem;
+}
